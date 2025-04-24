acd97af75cdcc4e613006fa49fc0abd99a1619d5
idan <idandrori555@gmail.com>
2025-04-24 22:51:29 +0300

    updated script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const projects = [
     title: "Moovit Bus Pass",
     description:
       "אפליקציה סטטית פשוטה שיוצרת כרטיס נסיעה לשעה ולתאריך הנוכחיים.",
-    technologies: ["HTML", "CSS", "JavaScript"],
+    technologies: ["HTML", "CSS", "JS"],
     icon: "M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7",
     link: "/projects/moovit_bus_pass/",
   },
@@ -15,7 +15,7 @@ const projects = [
     title: "Magshi Date",
     description:
       "אפליקציה נקייה ודוברת שתי שפות לבדיקת שיעורי מגשימים הקרובים לפי תאריך.",
-    technologies: ["HTML", "Tailwind", "JavaScript"],
+    technologies: ["HTML", "Tailwind", "JS"],
     icon: "M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z",
     link: "/projects/magshi-date/",
   },
@@ -26,7 +26,7 @@ const projects = [
     title: "Compiler Project",
     description:
       "קומפיילר בשביל הבגרות במדעי המחשב עם המחלקות של משרד החינוך לבגרות",
-    technologies: ["HTML", "CSS", "JavaScript", "Typescript", "Express"],
+    technologies: ["HTML", "CSS", "JS", "Deno", "Typescript", "Express"],
     icon: "M4 6a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2H6a2 2 0 01-2-2V6z M12 8a4 4 0 100 8 4 4 0 000-8zm0-2v2 M12 16v2 M8 12H6 M18 12h-2 M15.657 8.343l-1.414 1.414 M9.757 14.243l-1.414 1.414 M15.657 15.657l-1.414-1.414 M9.757 9.757l-1.414-1.414",
     link: "https://github.com/idandrori555/Online-Edu-Java-Compiler",
   },
@@ -36,7 +36,7 @@ const projects = [
     category: "פורטפוליו",
     title: "Terminal Portfolio",
     description: "פורטפוליו בסיסי עם עיצוב של טרמינל וכמה פקודות",
-    technologies: ["HTML", "CSS", "JavaScript"],
+    technologies: ["HTML", "CSS", "JS"],
     icon: "M3 5a2 2 0 012-2h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5zm4.5 4.5l3 3-3 3M13 12h4",
     link: "/projects/terminal-portfolio",
   },
@@ -73,8 +73,8 @@ const projectTemplate = (project) => `
             <p class="text-gray-600 mb-6">
                 ${project.description}
             </p>
-            <div class="flex justify-between items-center">
-                <div class="flex space-x-2 space-x-reverse">
+            <div class="flex justify-between items-center flex-wrap gap-y-2">
+                <div class="flex flex-wrap gap-2">
                     ${project.technologies
                       .map(
                         (tech) => `
