a08f73c22d3d935c96d95911721ebc35f4711217
idan <idandrori555@gmail.com>
2026-05-15 17:32:35 +0300

    updated index.html for portfolio

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const projects = [
     category: "קומפיילר",
     title: "MOE Compiler Project (Wrapper)",
     description: "סביבת הרצה וקומפילציה אונליין שנבנתה במיוחד עבור תלמידי מדעי המחשב, התומכת במחלקות ובספריות הרשמיות של משרד החינוך לבגרות.",
-    technologies: ["Deno", "TypeScript", "Express", "Docker", "Java CLI"],
+    technologies: ["Bun", "TypeScript", "Express", "Docker", "Java CLI"],
     icon: "M7 8l-4 4l4 4 M17 8l4 4l-4 4 M14 4l-4 16",
     link: "https://github.com/idandrori555/Edu-Compiler",
   },
